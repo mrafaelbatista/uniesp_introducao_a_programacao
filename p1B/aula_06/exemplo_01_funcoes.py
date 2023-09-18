8dee1df279e5057720cd3c5742fa728661b0094e
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2023-09-18 20:02:11 -0300

    Aula 18/09/2023 - Funções para aprender teste

---
+++ p1B/aula_06/exemplo_01_funcoes.py
@@ -0,0 +1,21 @@
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
+
+
+def somar(numero1, numero2):
+    return numero1 + numero2
+
+
+def subtrair(numero1, numero2):
+    return numero1 - numero2
+
+
+def dividir(numero1, numero2):
+    return numero1 / numero2
+
+
+print(f'Multiplicar: {multiplicar(numero1=2, numero2=3)}')
+print(f'Somar: {somar(numero1=2, numero2=3)}')
+print(f'Subtrair: {subtrair(numero1=2, numero2=3)}')
+print(f'Dividir: {dividir(numero1=2, numero2=3)}')
+print('Fim do programa')
---
+++ teste/consumo_api.py
@@ -0,0 +1,13 @@
+import json
+import requests
+from deep_translator import GoogleTranslator
+
+url = 'https://api.adviceslip.com/advice'
+
+result = requests.request("GET", url)
+lista = json.loads(result.text)
+texto1 = lista["slip"]["advice"]
+print(texto1)
+
+texto = GoogleTranslator(source='english', target='portuguese').translate(texto1)
+print(texto)
