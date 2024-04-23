856a9610f76b0288bdeee502f8940609660a15ef
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2024-04-22 21:02:42 -0300

    Aula 08 P1B

--- aula_08/p1b/sample.py
+++ aula_08/p1b/sample.py
@@ -9,16 +9,24 @@ def adicionar_vingador():
 
 def listar_vingadores():
     print("------- Lista dos Vingadores -------")
-    for v in vingadores:
-        print(v)
-
+    tamanho_da_lista = len(vingadores)
+    for i in range(tamanho_da_lista):
+        print(f'{i} - {vingadores[i]}')
     print("------- Fim da Lista dos Vingadores -------")
+
+
+def alterar_vingador():
+    listar_vingadores()
+    n_vingador = int(input('Escolhar o vingador a ser alterado: '))
+    vingadores[n_vingador] = input('Digite o novo nome do Vingador: ')
+    print("------- Fim da Alteração do Vingador -------")
 
 
 while status:
     print('0 - Sair')
     print('1 - Adicionar Vingador')
     print('2 - Listar Vingadores')
+    print('3 - Alterar Vingador')
     op = int(input('Digite a opção desejada: '))
 
     if op == 0:
@@ -27,5 +35,7 @@ while status:
         adicionar_vingador()
     elif op == 2:
         listar_vingadores()
+    elif op == 3:
+        alterar_vingador()
     else:
         print('Digite uma opção válida!')
