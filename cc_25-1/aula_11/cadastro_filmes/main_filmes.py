48b7ef83b4b439b1258ceec5b695fc845aef3c57
Messi Batista <mrafaelbatista@gmail.com>
2025-05-07 21:20:40 -0300

    Cadastro de Filmes

--- cc_25-1/aula_11/cadastro_filmes/main_filmes.py
+++ cc_25-1/aula_11/cadastro_filmes/main_filmes.py
@@ -1,4 +1,8 @@
 import time
+from email.encoders import encode_noop
+
+from loguru import logger
+
 
 bd_filmes = [
     ['Matrix', 1997],
@@ -19,14 +23,31 @@ def altera_filme(bd, indice, titulo, ano):
     bd[indice][1] = ano
     return bd
 
+def salvar_filmes(bd):
+
+    with open('bd_filmes.txt', 'w', encoding='utf-8') as arquivo:
+
+        for i in range(len(bd)):
+            logger.info(f'Salvando o filme {bd[i][0]}')
+            arquivo.write(f'{bd[i][1]},{bd[i][0]}\n')
+
 
 while True:
     print('1 - Cadastrar Filme')
     print('2 - Listar Filmes')
     print('3 - Alterar Filme')
-    op = int(input('Digite sua opção: '))
+    print('4 - Salvar Filmes')
+
+    try:
+        op = int(input('Digite sua opção: '))
+    except Exception as e:
+        logger.error(f'Error: {e}')
+        logger.info('Digite um valor numérico!')
+        op = -1
 
     if op == 1:
+        logger.info('Iniciando o cadastro de filme')
+
         titulo = input('Digite o título do filme: ')
         ano = int(input('Digite o ano do filme: '))
 
@@ -36,14 +57,15 @@ while True:
             ano=ano
         )
 
-        print('Filme cadastrado!')
+        logger.info('Filme cadastrado com sucesso!')
 
     elif op == 2:
-
+        logger.info('Iniciando listagem de filmes')
         lista_filmes(bd_filmes)
-        print('Filmes listados!')
+        logger.info('Filmes listados com sucesso!')
 
     elif op == 3:
+        logger.info('Iniciando alteração de filme')
         lista_filmes(bd_filmes)
         i = int(input('Qual filme deseja alterar? '))
         titulo = input('Digite o novo título: ')
@@ -54,7 +76,13 @@ while True:
             titulo=titulo,
             ano=ano
         )
-        print('Filme alterado!')
+        logger.info('Filme alterado com sucesso!')
+
+    elif op == 4:
+        logger.info('Iniciando persistência dos filmes')
+        salvar_filmes(bd_filmes)
+        logger.info('Filmes salvos com sucesso!')
+
     else:
         print(f'Opção {op} inválida!')
 
